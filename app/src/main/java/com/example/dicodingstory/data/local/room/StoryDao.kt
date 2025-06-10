6277780e1a2e8a49c3e3df4732dfa1b176716049
zakymzn <mamurzakynurrokhman@gmail.com>
2025-06-10 05:32:59 +0000

    menambahkan fungsi baru untuk mendapatkan semua story dan menambahkan story ke local

--- app/src/main/java/com/example/dicodingstory/data/local/room/StoryDao.kt
+++ app/src/main/java/com/example/dicodingstory/data/local/room/StoryDao.kt
@@ -1,8 +1,17 @@
 package com.example.dicodingstory.data.local.room
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.dicodingstory.data.local.entity.StoryEntity
 
 @Dao
 interface StoryDao {
+    @Query("SELECT * FROM story")
+    fun getAllStories(): LiveData<List<StoryEntity>>
 
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertStory(event: List<StoryEntity>)
 }
