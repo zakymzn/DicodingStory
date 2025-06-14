96d65266be91e349c5bcf3d66e986fc011be4621
zakymzn <mamurzakynurrokhman@gmail.com>
2025-06-14 12:22:21 +0700

    mengubah nama StoryBasicResponse menjadi StoryErrorResponse

--- app/src/main/java/com/example/dicodingstory/data/remote/response/StoryErrorResponse.kt
+++ app/src/main/java/com/example/dicodingstory/data/remote/response/StoryErrorResponse.kt
@@ -2,7 +2,7 @@ package com.example.dicodingstory.data.remote.response
 
 import com.google.gson.annotations.SerializedName
 
-data class StoryBasicResponse(
+data class StoryErrorResponse(
 
 	@field:SerializedName("error")
 	val error: Boolean? = null,
