76847171534983e2744217668201a9678c4b718e
zakymzn <mamurzakynurrokhman@gmail.com>
2025-07-04 10:36:33 +0700

    menghapus file MainAdapter.kt MainViewModel.kt MainViewModelFactory.kt

--- app/src/main/java/com/example/dicodingstory/ui/main/MainViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.dicodingstory.ui.main
-
-import androidx.lifecycle.ViewModel
-import com.example.dicodingstory.data.StoryRepository
-
-class MainViewModel(private val storyRepository: StoryRepository) : ViewModel() {
-    fun getAllStories() = storyRepository.getAllStories()
-}
