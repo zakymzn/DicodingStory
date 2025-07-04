7ed1b192248e978b5d9bd6e9f53ae4c1d42e39f3
zakymzn <mamurzakynurrokhman@gmail.com>
2025-07-04 10:37:19 +0700

    menghapus import yang tidak dibutuhkan

--- app/src/main/java/com/example/dicodingstory/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/dicodingstory/ui/main/MainActivity.kt
@@ -1,27 +1,15 @@
 package com.example.dicodingstory.ui.main
 
-import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.Toast
-import androidx.activity.viewModels
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.GridLayoutManager
-import com.example.dicodingstory.R
-import com.example.dicodingstory.data.Result
 import com.example.dicodingstory.databinding.ActivityMainBinding
-import com.example.dicodingstory.ui.account.AccountActivity
 import com.example.dicodingstory.ui.story.StoryActivity
-import com.example.dicodingstory.ui.upload.UploadActivity
 import com.example.dicodingstory.ui.welcome.WelcomeActivity
 import com.example.dicodingstory.utils.SessionManager
 import com.example.dicodingstory.utils.dataStore
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
