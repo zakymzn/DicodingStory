cdb611e6d25b938831e250714b2665d2e9f87cc6
zakymzn <mamurzakynurrokhman@gmail.com>
2025-08-30 13:31:08 +0700

    perbaikan ui test untuk MainActivityTest.kt

--- app/src/androidTest/java/com/example/dicodingstory/ui/main/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/dicodingstory/ui/main/MainActivityTest.kt
@@ -1,10 +1,7 @@
 package com.example.dicodingstory.ui.main
 
-import android.view.View
 import androidx.test.espresso.Espresso.*
 import androidx.test.espresso.IdlingRegistry
-import androidx.test.espresso.UiController
-import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -18,7 +15,6 @@ import androidx.test.filters.LargeTest
 import com.example.dicodingstory.R
 import com.example.dicodingstory.ui.login.LoginActivity
 import com.example.dicodingstory.utils.EspressoIdlingResource
-import org.hamcrest.Matcher
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -34,51 +30,29 @@ class MainActivityTest {
 
     @Before
     fun setUp() {
+        Intents.init()
         IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
     }
 
     @After
     fun tearDown() {
+        Intents.release()
         IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
     }
 
     @Test
-    fun login_Success() {
-        Intents.init()
-        onView(withId(R.id.button_login)).check(matches(isDisplayed()))
-        onView(withId(R.id.button_register)).check(matches(isDisplayed()))
-        onView(withId(R.id.button_login)).perform(click())
+    fun login_logout_Success() {
+        onView(withId(R.id.button_login)).check(matches(isDisplayed())).perform(click())
 
         intended(hasComponent(LoginActivity::class.java.name))
-        onView(withId(R.id.ed_login_email)).check(matches(isDisplayed()))
-        onView(withId(R.id.ed_login_email)).perform(typeText("zaky@mail.com"))
+        onView(withId(R.id.ed_login_email)).check(matches(isDisplayed())).perform(typeText("zaky@mail.com"))
         closeSoftKeyboard()
-        onView(withId(R.id.ed_login_password)).check(matches(isDisplayed()))
-        onView(withId(R.id.ed_login_password)).perform(typeText("zakystory"))
+        onView(withId(R.id.ed_login_password)).check(matches(isDisplayed())).perform(typeText("zakystory"))
         closeSoftKeyboard()
-        onView(withId(R.id.button_login)).check(matches(isDisplayed()))
-        onView(withId(R.id.button_login)).perform(click())
+        onView(withId(R.id.button_login)).check(matches(isDisplayed())).perform(click())
 
-        onView(isRoot()).perform(waitFor(5000))
-        onView(withId(R.id.action_logout)).check(matches(isDisplayed()))
-        onView(withId(R.id.action_logout)).perform(click())
+        onView(withId(R.id.action_logout)).check(matches(isDisplayed())).perform(click())
 
         onView(withId(R.id.tv_welcome_title)).check(matches(isDisplayed()))
-    }
-}
-
-fun waitFor(delay: Long): ViewAction? {
-    return object : ViewAction {
-        override fun getConstraints(): Matcher<View> {
-            return isRoot()
-        }
-
-        override fun getDescription(): String? {
-            return "wait for $delay milliseconds"
-        }
-
-        override fun perform(uiController: UiController, view: View?) {
-            uiController.loopMainThreadForAtLeast(delay)
-        }
     }
 }
